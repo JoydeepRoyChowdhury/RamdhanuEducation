4d72e3081a0e05d6ea075a343ec12f66fa06dc68
JoydeepRoyChowdhury <joydeeprc@gmail.com>
2015-09-30 10:58:58 +0530

    colours and contents

--- RamdhanuEducation/js/ramdhanu.js
+++ RamdhanuEducation/js/ramdhanu.js
@@ -1,3 +1,13 @@
+
+
+function getAColour() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 function getContent(fileName) {
 	var jqXHR = $.ajax({
 		async : false,
